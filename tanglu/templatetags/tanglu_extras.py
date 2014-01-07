2bf2559a7f0538e55f1f9f0673a7c4c977e754d6
Éverton Arruda <e.arruda@samsung.com>
2014-01-07 11:45:24 -0400

    Added try-catch to avoid exception in feed parsing method
    Signed-off-by: Éverton Arruda <e.arruda@samsung.com>

--- tanglu/templatetags/tanglu_extras.py
+++ tanglu/templatetags/tanglu_extras.py
@@ -8,7 +8,7 @@ register = template.Library()
 def safewrap(val, arg):
     return val.format(arg)
 
-register.filter('safewrap', safewrap) 
+register.filter('safewrap', safewrap)
 safewrap.is_safe = True
 
 # Feed Parser template tag
@@ -25,32 +25,37 @@ def feed(feedLocation):
     # Parse the feed XML
     feedData = feedparser.parse(feedLocation)
 
-    # Check the HTTP status. If not 200, return a default Feed object
-    if feedData.status != 200:
-        return Feed()
-
     # Instantiate a Feed object
     feed = Feed()
-    feed.title = feedData.feed.title
-    feed.link = feedData.feed.link
 
-    # Append the entries to the Feed object
-    feed.entries = []
-    for entry in feedData.entries:
-        if maxEntries == 0:
-            break
+    # Check the HTTP status. If not 200, return a default Feed object
+    try:
+        if feedData.status != 200:
+            return Feed()
 
-        feed.entries.append({
-            'title': entry.title,
-            'link': entry.link,
-            'published': entry.published,
-        })
+        # Set feed title and link attributes
+        feed.title = feedData.feed.title
+        feed.link = feedData.feed.link
 
-        maxEntries -= 1
+        # Append the entries to the Feed object
+        feed.entries = []
+        for entry in feedData.entries:
+            if maxEntries == 0:
+                break
 
-    # Set hasEntries to True in Feed object
-    if feed.entries:
-        feed.hasEntries = True
+            feed.entries.append({
+                'title': entry.title,
+                'link': entry.link,
+                'published': entry.published,
+            })
+
+            maxEntries -= 1
+
+        # Set hasEntries to True in Feed object
+        if feed.entries:
+            feed.hasEntries = True
+    except:
+        pass
 
     # Return the feed object
     return feed
