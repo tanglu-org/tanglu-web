21811b0fb687b036b3d7ca0c0e061a5d5f9bbcad
Matthias Klumpp <matthias@tenstral.net>
2014-11-02 19:21:38 +0100

    Update to Mezzanine-based CMS

--- menulinks/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from menulinks.models import MenuLink
-
-class MenuLinkAdmin(admin.ModelAdmin):
-    """View for the Admin page"""
-    list_display = ('title', 'url', 'weight')
-
-admin.site.register(MenuLink, MenuLinkAdmin)
--- menulinks/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class MenuLink(models.Model):
-    title = models.CharField(max_length=50)
-    url = models.CharField(max_length=200)
-    weight = models.IntegerField(default=0)
-
-    def __unicode__(self):
-        return self.title
